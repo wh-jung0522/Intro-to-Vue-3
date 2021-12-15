bc5f12451b40d8e0cf3a26fdcce15e6a66fa7ea6
whjung <kwdg1213@gmail.com>
2021-12-15 18:18:17 +0900

    [ch5]

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants:[
+                {id:1234, color:'green'},
+                {id:5678, color:'blue'}
+            ]
         }
     }
 })
