b2edba1444489b7c2c3d1efbfa3995af1cd31506
whjung <kwdg1213@gmail.com>
2021-12-16 13:48:04 +0900

    [ch6]

--- main.js
+++ main.js
@@ -11,5 +11,17 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+    methods:{
+        addToCart(){
+            this.cart += 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        },
+        decreseToCart(){
+            this.cart -= 1
+        }
+        
     }
 })
